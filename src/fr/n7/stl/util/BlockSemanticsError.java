ad396c0c444f0ba5369f96eccd4db233be6d93a2
unknown <yygolivia@gmail.com>
2025-06-15 21:17:27 +0200

    MiniJava+MiniC

--- src/fr/n7/stl/util/BlockSemanticsError.java
+++ src/fr/n7/stl/util/BlockSemanticsError.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package fr.n7.stl.util;
 
@@ -9,49 +9,49 @@ package fr.n7.stl.util;
  */
 public class BlockSemanticsError extends RuntimeException {
 
-	private static final long serialVersionUID = 2853370029801143645L;
+    private static final long serialVersionUID = 2853370029801143645L;
 
-	/**
-	 * 
-	 */
-	public BlockSemanticsError() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     *
+     */
+    public BlockSemanticsError() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param message
-	 */
-	public BlockSemanticsError(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param message
+     */
+    public BlockSemanticsError(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param cause
-	 */
-	public BlockSemanticsError(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param cause
+     */
+    public BlockSemanticsError(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public BlockSemanticsError(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param message
+     * @param cause
+     */
+    public BlockSemanticsError(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public BlockSemanticsError(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace
+     */
+    public BlockSemanticsError(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
 
 }
